f49551f297d70bb64ab65b1030f32bac0cca1e0a
Thelma Akpata <50399915+Thelma101@users.noreply.github.com>
2023-11-08 16:48:02 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// function scrambleWords() {
-//     const originalText = document.getElementById("originalText").value;
-//     const wordToScramble = document.getElementById("wordToScramble").value;
+function scrambleWords() {
+    const originalText = document.getElementById("originalText").value;
+    const wordToScramble = document.getElementById("wordToScramble").value;
 
-//     if (!originalText || !wordToScramble) {
-//         alert("Please enter both text and word to scramble.");
-//         return;
-//     }
+    if (!originalText || !wordToScramble) {
+        alert("Please enter both text and word to scramble.");
+        return;
+    }
 
-//     // Create a regex pattern to match the word globally and case-insensitively
-//     const wordPattern = new RegExp(`\\b${wordToScramble}\\b`, "gi");
+    // Create a regex pattern to match the word globally and case-insensitively
+    const wordPattern = new RegExp(`\\b${wordToScramble}\\b`, "gi");
 
-//     // Scramble the text by replacing the matched word with asterisks of the same length
-//     const scrambledText = originalText.replace(wordPattern, match => '*'.repeat(match.length));
+    // Scramble the text by replacing the matched word with asterisks of the same length
+    const scrambledText = originalText.replace(wordPattern, match => '*'.repeat(match.length));
 
-//     // Update the content of the "scrambledText" element
-//     const scrambledTextElement = document.getElementById("scrambledText");
-//     scrambledTextElement.textContent = scrambledText;
-// }
+    // Update the content of the "scrambledText" element
+    const scrambledTextElement = document.getElementById("scrambledText");
+    scrambledTextElement.textContent = scrambledText;
+}
